44e3ad286d44ad7baab63092cebbfcb6d21e05bf
Nafoam <geonoangay@gmail.com>
2023-08-01 14:32:05 +0700

    fix the not 'vite' available

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,3 @@ Route::get('/', function () {
 Route::get('/register', function () {
     return view('register');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
