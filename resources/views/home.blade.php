c8736a7e6679c14813a52ddfcc79dc9a80271292
Nafoam <geonoangay@gmail.com>
2023-08-01 15:55:40 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">New To-do-list</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -36,12 +36,40 @@
         </div>
     </div>
 
+<!-- modal for update  -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Update</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Title</label>
+                    <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="mb-3">
+                    <label for="message-text" class="col-form-label">Description</label>
+                    <textarea class="form-control" id="message-text"></textarea>
+                </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Add to list</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-                
-
+            
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -52,6 +80,7 @@
                     {{ __('You are logged in!') }}
                 </div>
 
+<!-- table for to do list -->
                 <table class="table">
                 <thead>
                     <tr>
@@ -70,8 +99,13 @@
                     <td>Pending</td>
                   
                     <td>
-                        <a href="#" class="btn btn-warning btn-sm" data-toggle="modal"
-                                    data-target="#">Edit</a>
+                        
+                        <!-- <a href="#" class="btn btn-warning btn-sm" data-toggle="modal" 
+                            ata-target="#editModal">Edit</a> -->
+
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" 
+                            data-bs-target="#editModal">Add</button>
+
                         <a href="#" onclick="return confirm('Are Sure Want To Deleted ?')"
                             class="btn btn-danger btn-sm">Delete</a>
                     </td>
